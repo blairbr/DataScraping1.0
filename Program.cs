2c233098b4a289256eb99190e9a59002436abafc
blairbr <blair.j.brown@gmail.com>
2019-01-22 20:06:02 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ScrapingTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+			HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.yellowpages.com/search?search_terms=Software&geo_location_terms=Sydney2C+ND");
+			var HeaderNames = doc.DocumentNode.SelectNodes("//a[@class='business-name']").ToList();
+
+		Console.WriteLine(HeaderNames);
+			foreach (var item in HeaderNames)
+			{
+				Console.WriteLine(item.InnerText);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
